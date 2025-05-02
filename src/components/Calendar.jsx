9803303e00b9de2aaef5eaccfd83d9253baa2498
Stamich47 <159186195+Stamich47@users.noreply.github.com>
2025-05-02 14:04:26 -0700

    Add logic for changing view in Calendar component. When a user clicks on a certain date, the view should change to that date.

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,11 +1,13 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchEvents } from "./../services/slices/dataSlice";
+import { supabase } from "../services/supabaseClient";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from "@fullcalendar/list";
+import resourceTimelinePlugin from "@fullcalendar/resource-timeline";
 import bootstrapPlugin from "@fullcalendar/bootstrap";
 import "@fullcalendar/bootstrap5";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -18,11 +20,24 @@ export default function Calendar() {
     if (status === "idle") {
       dispatch(fetchEvents());
     }
-  }, [status, dispatch]);
 
-  if (status === "loading") {
-    return <p>Loading events...</p>;
-  }
+    const subscription = supabase
+      .channel("realtime:events")
+      .on(
+        "postgres_changes",
+        { event: "*", schema: "public", table: "events" },
+        (payload) => {
+          console.log("Change received!", payload);
+
+          dispatch(fetchEvents());
+        }
+      )
+      .subscribe();
+
+    return () => {
+      supabase.removeChannel(subscription);
+    };
+  }, [status, dispatch]);
 
   if (status === "failed") {
     return <p>Error: {error}</p>;
@@ -31,17 +46,27 @@ export default function Calendar() {
   return (
     <div className="calendar-container">
       <FullCalendar
+        schedulerLicenseKey="CC-Attribution-NonCommercial-NoDerivatives"
         plugins={[
           dayGridPlugin,
           timeGridPlugin,
           interactionPlugin,
           listPlugin,
           bootstrapPlugin,
+          resourceTimelinePlugin,
         ]}
+        dateClick={(info) => {
+          const currentView = info.view.calendar;
+          currentView.changeView("timeGridDay", info.dateStr);
+        }}
+        eventClick={(info) => {
+          const currentView = info.view.calendar;
+          currentView.changeView("timeGridDay", info.event.startStr);
+        }}
         initialView="dayGridMonth"
         headerToolbar={{
           left: "prev,next today",
-          center: "title",
+          center: `title`,
           right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
         }}
         themeSystem="bootstrap5"
